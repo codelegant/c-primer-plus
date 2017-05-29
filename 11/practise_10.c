ee6f6841979fd7e81cbf213c72b3f28359a80e84
codelegant <laichuanfeng@hotmail.com>
2017-05-29 16:28:48 +0800

    UPDATED

--- 11/practise_10.c
+++ 11/practise_10.c
@@ -4,49 +4,168 @@
 #define SIZE 80
 #define LIM 10
 char get_first(void);
+void shallow_copy(char * [], char [][SIZE], int);
+void sort_str(char * [], int);
+char * get_first_word(char *);
 char choice_menu(void);
-void print_source(char * []);
-void print_by_ascii(char * []);
-void print_by_length(char * []);
-void print_by_first_word_length(char * []);
+void print_divided_line(char);
+void print_source(char [][SIZE], int);
+void print_by_ascii(char [][SIZE], int);
+void print_by_length(char [][SIZE], int);
+void print_by_first_word_length(char [][SIZE], int);
 
 int main(void)
 {
     char input[LIM][SIZE];
     int ct=0;
     int index=0;
+    int choice;
 
     printf("Enter up to %d lines: \n", LIM);
-    while(ct<LIM && gets(input[ct])!=0 && input[ct][0]!=EOF)
+    while(ct<LIM && fgets(input[ct], SIZE, stdin)!=NULL && input[ct][0]!=EOF)
         ct++;
-    printf("String input end.\n");
+    printf("Source string input end.\n");
 
-    for(index=0; index<ct; index++)
+    while((choice=choice_menu()) != 'q')
     {
-        remove_whitespace(input[index]);
-        puts(input[index]);
+        print_divided_line('*');
+        switch(choice)
+        {
+            case 'a':print_source(input, ct);
+                     break;
+            case 'b':print_by_ascii(input, ct);
+                     break;
+            case 'c':print_by_length(input, ct);
+                     break;
+            case 'd':print_by_first_word_length(input, ct);
+                     break;
+            default:printf("Program error!\n");
+        }
+        print_divided_line('*');
     }
+    printf("Bye.\n");
     return 0;
 }
 
 char get_first(void)
 {
-    int ch;
-    while((ch = getchar()) && !isalpha(ch))
-        continue;
-
-    while(getchar() != '\n')
+    char ch;
+    while(((ch = getchar()) && !isalpha(ch)) || getchar() != '\n')
         continue;
 
     return tolower(ch);
 }
 
+void shallow_copy(char * source[], char target[][SIZE], int ct)
+{
+    int index=0; 
+    while(index<ct)
+    {
+        source[index]=target[index];
+        index++;
+    }
+}
+
+char * get_first_word(char * str)
+{
+    char * find=strchr(str, 32);
+    if(find)
+        *find='\0';
+    return str;
+}
+
 char choice_menu(void)
 {
-    int ch;
-    printf("请输入你的选择");
-    printf("a. 输出原始字符串列表            b. 按 ASCII 顺序输出字符串\n");
-    printf("c. 按长度递增顺序输出字符串      d. 按字符串中第一个单词的长度输出字符串\n");
-    printf("q. 退出\n");
+    char ch;
+    print_divided_line('-');
+    printf("Enter the letter of your choic:\n");
+    printf("a. Print the source string                     b. Print the string order by ASCII\n");
+    printf("c. Print the string order by string length     d. Print the string order by the first word's length of string\n");
+    printf("q. Quit\n");
+    print_divided_line('-'); 
+    printf("Your choice is: ");
+    ch=get_first();
+    while((ch<'a' || ch>'d') && ch!='q')
+    {
+        printf("Please respond with a, b, c, d or q.\n");
+        ch=get_first();
+    }
+    return ch;
+}
 
+void print_divided_line(char ch)
+{
+    int size=SIZE;
+    while(size--)
+        putchar(ch);
+    putchar('\n');
+}
+
+void print_source(char input[][SIZE], int ct)              
+{
+    int index;
+    for(index=0; index<ct; index++)
+        fputs(input[index], stdout);
+}
+
+void print_by_ascii(char input[][SIZE], int ct)
+{
+    char * cache[ct];
+    char * temp;
+    int top, seek;
+    int index=0;
+    shallow_copy(cache, input, ct);
+
+    for(top=0; top<ct-1; top++)
+        for(seek=top+1; seek<ct; seek++)
+            if(strcmp(cache[top], cache[seek])>0)
+            {
+                temp=cache[top];
+                cache[top]=cache[seek];
+                cache[seek]=temp;
+            }
+
+    while(index<ct)
+        fputs(cache[index++], stdout);
+}
+
+void print_by_length(char input[][SIZE], int ct)
+{
+    char * cache[ct];
+    char * temp;
+    int top, seek;
+    int index=0;
+    shallow_copy(cache, input, ct);
+
+    for(top=0; top<ct-1; top++)
+        for(seek=top+1; seek<ct; seek++)
+            if((int)strlen(cache[top])-(int)strlen(cache[seek]) > 0)
+            {
+                temp=cache[top];
+                cache[top]=cache[seek];
+                cache[seek]=temp;
+            }
+
+    while(index<ct)
+        fputs(cache[index++], stdout);
+}
+
+void print_by_first_word_length(char input[][SIZE], int ct)
+{
+    char * cache[ct];
+    char * temp;
+    int top, seek;
+    int index=0;
+    shallow_copy(cache, input, ct);
+
+    for(top=0; top<ct-1; top++)
+        for(seek=top+1; seek<ct; seek++)
+            if((int)strlen(get_first_word(cache[top]))-(int)strlen(get_first_word(cache[seek])) > 0)
+            {
+                temp=cache[top];
+                cache[top]=cache[seek];
+                cache[seek]=temp;
+            }
+    while(index<ct)
+        fputs(cache[index++], stdout);
 }
