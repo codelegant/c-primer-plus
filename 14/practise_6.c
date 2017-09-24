da734ca9ea545ab05f23b2c9b512398c9ef2362b
chuanfeng <laichunanfeng@hotmail>
2017-09-24 22:15:31 +0800

    UPDATED /整形计算浮点型，计算数进行类型转换，不是计算后的结果进行转换

--- 14/practise_6.c
+++ 14/practise_6.c
@@ -4,6 +4,7 @@
 #define MAXNAME 40
 #define MEMEBERS 19
 #define LINES 100
+
 struct player {
   int number; // 球员号
   char firstname[MAXNAME];
@@ -14,6 +15,7 @@ struct player {
   int runs_batted_in; // 跑点数
   float batting_average; // 平均成功率 = hits / walks
 };
+
 void initTeam(struct player [], int);
 void getPlayerData(struct player [], int, char *);
 void computeBattingAverage(struct player [], int);
@@ -31,11 +33,11 @@ int main(void) {
     fputs("Catn't open player.dat file\n", stderr);
     exit(1);
   }
+
   rewind(p_book);
   initTeam(team, MEMEBERS);
-  while (fgets(line_data, LINES, p_book) != NULL) {
+  while (fgets(line_data, LINES, p_book) != NULL)
     getPlayerData(team, MEMEBERS, line_data);
-  }
   computeBattingAverage(team, MEMEBERS);
   showInfo(team, MEMEBERS);
 
@@ -44,10 +46,8 @@ int main(void) {
 
 void initTeam(struct player team[], int size) {
   int index = 0;
-  while (index < size) {
-    team[index].number = 0;
-    index++;
-  }
+  while (index < size)
+    team[index++].number = 0;
 }
 
 void getPlayerData(struct player team[], int size, char * line) {
@@ -56,28 +56,31 @@ void getPlayerData(struct player team[], int size, char * line) {
   char * units [units_length];
   int index = 0;
   while (pt) {
-    units[index] = pt;
+    units[index++] = pt;
     pt = strtok(NULL, " ");
-    index++;
   }
   index = 0;
   while (index < size) {
+    int number = atoi(units[0]);
     int times_at_bat = atoi(units[3]);
     int hits = atoi(units[4]);
     int walks = atoi(units[5]);
-    if (team[index].number == atoi(units[0])) {
-      team[index].times_at_bat = team[index].times_at_bat + times_at_bat;
-      team[index].hits = team[index].hits + hits;
-      team[index].walks = team[index].walks + walks;
+    int runs_batted_in = atoi(units[6]);
+    if (team[index].number == number) {
+      struct player cur = team[index];
+      team[index].times_at_bat = cur.times_at_bat + times_at_bat;
+      team[index].hits = cur.hits + hits;
+      team[index].walks = cur.walks + walks;
+      team[index].runs_batted_in = cur.runs_batted_in + runs_batted_in;
       break;
     } else if (!team[index].number) {
-      team[index].number = atoi(units[0]);
+      team[index].number = number;
       strcpy(team[index].firstname, units[1]);
       strcpy(team[index].lastname, units[2]);
       team[index].times_at_bat = times_at_bat;
       team[index].hits = hits;
       team[index].walks = walks;
-      team[index].runs_batted_in = atoi(units[6]);
+      team[index].runs_batted_in = runs_batted_in;
       break;
     }
     index ++;
@@ -87,8 +90,7 @@ void getPlayerData(struct player team[], int size, char * line) {
 void computeBattingAverage(struct player team[], int size) {
   int index = 0;
   while (index < size && team[index].number) {
-    struct player current = team[index];
-    team[index].batting_average = (float)(current.hits / current.walks);
+    team[index].batting_average = (float)team[index].hits / (float)team[index].walks;
     index++;
   }
 }
@@ -96,16 +98,14 @@ void computeBattingAverage(struct player team[], int size) {
 void showInfo(struct player team[], int size) {
   int index = 0;
   while (index < size && team[index].number) {
-    struct player current = team[index];
+    struct player current = team[index++];
     printf("Number is %d\n", current.number);
-    printf("Firstname is %s\n", current.firstname);
-    printf("Lastname is %s\n", current.lastname);
+    printf("Name is %s %s\n", current.firstname, current.lastname);
+    printf("Time at bat is %d\n", current.times_at_bat);
     printf("Hits is %d\n", current.hits);
     printf("Walks is %d\n", current.walks);
     printf("Runs batted in is %d\n", current.runs_batted_in);
     printf("Batting average is %.2f\n", current.batting_average);
-    printf("Time at bat is %d\n", current.times_at_bat);
-    printf("-------------------------------------------------------\n");
-    index++;
+    printf("\n");
   }
 }
